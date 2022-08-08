3dd1f6f5f3a1f851cccd8f8e81acc630e2422909
Shubh <shubh.scb@gmail.com>
2022-08-08 20:38:18 +0530

    TABLE CREATE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(200) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INTEGER NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL,
+    species_id INTEGER NOT NULL,
+    owner_id INTEGER NOT NULL
 );
