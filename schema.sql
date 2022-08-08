2e10712f88bf4cbd268a864471b0aab8a93e3f24
Shubh <shubh.scb@gmail.com>
2022-08-08 21:41:11 +0530

    DATA ADD

--- schema.sql
+++ schema.sql
@@ -6,7 +6,5 @@ CREATE TABLE animals (
     date_of_birth DATE NOT NULL,
     escape_attempts INTEGER NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL,
-    species_id INTEGER NOT NULL,
-    owner_id INTEGER NOT NULL
+    weight_kg DECIMAL NOT NULL
 );
