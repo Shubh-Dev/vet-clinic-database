5207bd28040534a7191444ad6a8e5fc95540aa30
Shubh <shubh.scb@gmail.com>
2022-08-09 18:29:21 +0530

    WRONG ENTRY CORRECT

--- data.sql
+++ data.sql
@@ -15,16 +15,16 @@ VALUES ('Devimon', '2017-05-12', 5, true, 11);
 -- day-2 
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Charmander', '2020-02-08', 0, false, 11);
+VALUES ('Charmander', '2020-02-08', 0, false, -11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Plantmon', '2021-11-15', 2, true, 5.7);
+VALUES ('Plantmon', '2021-11-15', 2, true, -5.7);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Squirtle', '1993-04-02', 3, false, 12.13);
+VALUES ('Squirtle', '1993-04-02', 3, false, -12.13);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Angemon', '2005-06-12', 1, true, 45);
+VALUES ('Angemon', '2005-06-12', 1, true, -45);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
